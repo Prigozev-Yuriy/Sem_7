48301e3cfee8d16df746f556aa96df68f9f1fa02
Prigozev-Yuriy <138002979+Prigozev-Yuriy@users.noreply.github.com>
2023-07-20 18:15:01 +0300

    Task 50 Commit

--- Homework/Task_47/Program.cs
+++ Homework/Task_47/Program.cs
@@ -1,38 +1,38 @@
-﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+﻿// // Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-// m = 3, n = 4.
-// 0,5 7 -2 -0,2
-// 1 -3,3 8 -9,9
-// 8 7,8 -7,1 9
+// // m = 3, n = 4.
+// // 0,5 7 -2 -0,2
+// // 1 -3,3 8 -9,9
+// // 8 7,8 -7,1 9
 
-double[,] GetMatrix(int m, int n)
-{
-    double[,] matrix = new double[m, n];
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        // i, k, m, n, j
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = Math.Round(new Random().NextDouble() * 100, 2);
-        }
-    }
-    return matrix;
-}
-void PrintMatrix(double[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            Console.Write(matrix[i, j] + "\t");
-        }
-        Console.WriteLine();
-    }
-}
-Console.Write("Введите количество строк: ");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите количество cтолбцов: ");
-int columns = Convert.ToInt32(Console.ReadLine());
-double[,] array2D = GetMatrix(rows, columns);
-Console.WriteLine("Матрица: ");
-PrintMatrix(array2D);
+// double[,] GetMatrix(int m, int n)
+// {
+//     double[,] matrix = new double[m, n];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         // i, k, m, n, j
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = Math.Round(new Random().NextDouble() * 100, 2);
+//         }
+//     }
+//     return matrix;
+// }
+// void PrintMatrix(double[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write(matrix[i, j] + "\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// Console.Write("Введите количество строк: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество cтолбцов: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// double[,] array2D = GetMatrix(rows, columns);
+// Console.WriteLine("Матрица: ");
+// PrintMatrix(array2D);
---
+++ Homework/Task_50/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n]; 
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество cтолбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите мин число: ");
+int minRandom = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите макс число: ");
+int maxRandom = Convert.ToInt32(Console.ReadLine());
+int[,] array = GetMatrix(rows, columns, minRandom, maxRandom);
+
+Console.Write("Введите строку искомого элемента: ");
+int line = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите столбец искомого элемента: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+if (line>=0 & line<=rows & column>=0 & column<=columns)
+{
+Console.Write($"Искомый элемент: {array[line, column]}");
+}
+else Console.Write($"Такого числа в массиве нет");
