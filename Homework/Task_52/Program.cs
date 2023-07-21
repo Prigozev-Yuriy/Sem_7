bcc368fe99be0a682c3d3f664a0212a508a89572
Prigozev-Yuriy <138002979+Prigozev-Yuriy@users.noreply.github.com>
2023-07-21 16:58:27 +0300

    Task_52

---
+++ Homework/Task_52/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// using System.Globalization;
+// using System.Runtime.Serialization.Formatters;
+
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+Console.Write("Введите количество строк массива: ");
+int lines = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальный элемент массива: ");
+int minimum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальный элемент массива: ");
+int maximum = Convert.ToInt32(Console.ReadLine());
+
+int[,] newMatrix = GetMatrix(lines, columns, minimum, maximum);
+PrintMatrix(newMatrix);
+int lengt = newMatrix.GetLength(1);
+Console.WriteLine("Среднее арифметическое каждого столбца: ");
+
+int[] sum = new int[newMatrix.GetLength(1)];
+for (int i = 0; i < newMatrix.GetLength(1); i++)
+{
+    for (int j = 0; j < newMatrix.GetLength(0); j++)
+    {
+        sum[i] = sum[i] + newMatrix[j, i];
+    }
+}
+
+void PrintAverage(int[] sum)
+{
+    for (int i = 0; i < sum.Length; i++)
+    {
+
+        Console.Write(Math.Round(Convert.ToDouble(sum[i]) / newMatrix.GetLength(0), 2) + "\t");
+    }
+
+}
+PrintAverage(sum);
--- Program.cs
+++ Program.cs
@@ -1,87 +1,87 @@
-﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
-// m = 3, n = 4.
-// 1 4 8 19
-// 5 -2 33 -2
-// 77 3 8 1
+﻿// // Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// // m = 3, n = 4.
+// // 1 4 8 19
+// // 5 -2 33 -2
+// // 77 3 8 1
 
-Console.Write("Введите количество строк: ");
-int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество строк: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите количество cтолбцов: ");
-int columns = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество cтолбцов: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите мин число: ");
-int minRandom = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите макс число: ");
-int maxRandom = Convert.ToInt32(Console.ReadLine());
-int[,] array2D = GetMatrix(rows, columns, minRandom, maxRandom);
-Console.WriteLine("Матрица: ");
-// Создается таблица из rows строчек и columns столбцов; minRandom - 0, maxRandom = 10
-PrintMatrix(array2D);
+// Console.Write("Введите мин число: ");
+// int minRandom = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите макс число: ");
+// int maxRandom = Convert.ToInt32(Console.ReadLine());
+// int[,] array2D = GetMatrix(rows, columns, minRandom, maxRandom);
+// Console.WriteLine("Матрица: ");
+// // Создается таблица из rows строчек и columns столбцов; minRandom - 0, maxRandom = 10
+// PrintMatrix(array2D);
 
-// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
-// m = 3, n = 4.
-// 0 1 2 3
-// 1 2 3 4
-// 2 3 4 5
+// // Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// // m = 3, n = 4.
+// // 0 1 2 3
+// // 1 2 3 4
+// // 2 3 4 5
 
-// Матрица - прямоугольная таблица размером m(строчки) на n(cтолбцов)
-int[,] GetMatrix(int m, int n, int min, int max)
-{
-    int[,] matrix = new int[m, n]; // [кол-во_строк, кол-во_столбцов]
-    for (int i = 0; i < matrix.GetLength(0); i++) // m, цикл по строчкам
-    {
-        // i, k, m, n, j
-        for (int j = 0; j < matrix.GetLength(1); j++)// n, цикл по столбцам
-        {
-            matrix[i, j] = new Random().Next(min, max + 1);
-            // matrix[i, j] = i + j; //Amn = m + n; m = i; n = j => Aij = i + j
-        }
-    }
-    return matrix;
-}
+// // Матрица - прямоугольная таблица размером m(строчки) на n(cтолбцов)
+// int[,] GetMatrix(int m, int n, int min, int max)
+// {
+//     int[,] matrix = new int[m, n]; // [кол-во_строк, кол-во_столбцов]
+//     for (int i = 0; i < matrix.GetLength(0); i++) // m, цикл по строчкам
+//     {
+//         // i, k, m, n, j
+//         for (int j = 0; j < matrix.GetLength(1); j++)// n, цикл по столбцам
+//         {
+//             matrix[i, j] = new Random().Next(min, max + 1);
+//             // matrix[i, j] = i + j; //Amn = m + n; m = i; n = j => Aij = i + j
+//         }
+//     }
+//     return matrix;
+// }
 
-void PrintMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++) // m, цикл по строчкам
-    {
-        // i, k, m, n, j
-        for (int j = 0; j < matrix.GetLength(1); j++)// n, цикл по столбцам
-        {
-            Console.Write(matrix[i, j] + "\t"); // Tab между элементами матрицы
-        }
-        Console.WriteLine();
-    }
-}
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++) // m, цикл по строчкам
+//     {
+//         // i, k, m, n, j
+//         for (int j = 0; j < matrix.GetLength(1); j++)// n, цикл по столбцам
+//         {
+//             Console.Write(matrix[i, j] + "\t"); // Tab между элементами матрицы
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
-// Задача 49: Задайте двумерный массив. Найдите элементы,
-//  у которых оба индекса чётные, и замените эти элементы на их квадраты.
-// Например, изначально массив
-//  выглядел вот так:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
+// // Задача 49: Задайте двумерный массив. Найдите элементы,
+// //  у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// // Например, изначально массив
+// //  выглядел вот так:
+// // 1 4 7 2
+// // 5 9 2 3
+// // 8 4 2 4
 
-int[,] ChangeMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i += 2) // строчки: i = i + 2
-    {
-        for (int j = 0; j < matrix.GetLength(1); j += 2) // столбцы: j = j + 2
-        {
-            // if (i % 2 == 0 && j % 2 == 0) // Проверяю, что оба индекса - четные числа
-            // {
-            matrix[i, j] *= matrix[i, j]; // matrix[i,j] = matrix[i,j] * matrix[i,j]
-            // }
-        }
-    }
-    return matrix;
-}
-Console.WriteLine("Результат: ");
+// int[,] ChangeMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i += 2) // строчки: i = i + 2
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j += 2) // столбцы: j = j + 2
+//         {
+//             // if (i % 2 == 0 && j % 2 == 0) // Проверяю, что оба индекса - четные числа
+//             // {
+//             matrix[i, j] *= matrix[i, j]; // matrix[i,j] = matrix[i,j] * matrix[i,j]
+//             // }
+//         }
+//     }
+//     return matrix;
+// }
+// Console.WriteLine("Результат: ");
 
-// 1. Без сохранения результата вызова метода ChangeMatrix(array2D)
-PrintMatrix(ChangeMatrix(array2D));
+// // 1. Без сохранения результата вызова метода ChangeMatrix(array2D)
+// PrintMatrix(ChangeMatrix(array2D));
 
-// 2. Результат лежит в change2DArray, его с помощью PrintMatrix и печатаю
-int[,] change2DArray = ChangeMatrix(array2D);
+// // 2. Результат лежит в change2DArray, его с помощью PrintMatrix и печатаю
+// int[,] change2DArray = ChangeMatrix(array2D);
 
-PrintMatrix(change2DArray);
+// PrintMatrix(change2DArray);
